cb0252daa9cd9557530364921bda7f4b58face2a
Gaurav Karki <karkigaurav490@gmail.com>
2023-10-08 21:35:22 +0530

    media query for Login page added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     <div>
       <Header />
       <div
-        className="absolute bg-fixed bg-cover bg-center w-screen h-screen"
+        className="absolute bg-fixed bg-cover bg-center w-full h-screen"
         style={{
           backgroundImage:
             "url('https://assets.nflxext.com/ffe/siteui/vlv3/893a42ad-6a39-43c2-bbc1-a951ec64ed6d/1d86e0ac-428c-4dfa-9810-5251dbf446f8/IN-en-20231002-popsignuptwoweeks-perspective_alpha_website_large.jpg')",
@@ -32,9 +32,9 @@ const Login = () => {
         onSubmit={(e) => {
           e.preventDefault();
         }}
-        className="absolute p-4 md:p-12 lg:p-16 bg-black w-full md:w-3/4 lg:w-2/5 xl:w-1/4 my-28 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-lg"
+        className="absolute p-4 md:p-12 lg:p-16 bg-black w-full md:w-3/4 lg:w-2/5 xl:w-1/3 my-16 mx-auto right-0 left-0 text-white bg-opacity-80 rounded-lg"
       >
-        <h1 className="font-bold text-3xl py-4">
+        <h1 className="font-bold text-3xl py-3">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
         {!isSignInForm && (
@@ -65,7 +65,7 @@ const Login = () => {
         )}
         <p className="text-red-600 font-bold py-2">{errorMessage}</p>
         <button
-          className="p-4 my-4 bg-red-600 w-full rounded-lg"
+          className="p-2 my-4 bg-red-600 w-full rounded-lg"
           onClick={handleButtonClick}
         >
           {isSignInForm ? "Sign In" : "Sign Up"}
